3471fbb115a44d845900760edcda4f76c06b60f7
Jonathan Ruiz Silva <ruizsilvajonathan@gmail.com>
2024-07-25 08:33:28 -0300

    validation with yup

--- src/components/post/PostEdit/PostEdit.tsx
+++ src/components/post/PostEdit/PostEdit.tsx
@@ -1,29 +1,71 @@
 import {
   Button,
+  Code,
   Group,
   Modal,
   SimpleGrid,
   Textarea,
   TextInput,
 } from "@mantine/core";
+import { useForm, yupResolver } from "@mantine/form";
 import { useDisclosure } from "@mantine/hooks";
 import { IconEdit } from "@tabler/icons-react";
+import * as yup from "yup";
+
+const schema = yup.object().shape({
+  title: yup.string().required("Titulo requerido"),
+  content: yup.string().required("Contenido requerido"),
+});
 
 interface Props {}
 
 export default function PostEdit(_props: Props): JSX.Element {
   const [opened, { open, close }] = useDisclosure(false);
+  const form = useForm({
+    mode: "uncontrolled",
+    initialValues: {
+      title: "123",
+      content: "444",
+    },
+    validate: yupResolver(schema),
+  });
 
   return (
     <>
       <Modal opened={opened} onClose={close} title="Editar publicación">
-        <SimpleGrid spacing={"xs"}>
-          <TextInput label="Titulo" placeholder="Input placeholder" />
-          <Textarea label="Contenido" placeholder="Input placeholder" />
-        </SimpleGrid>
-        <Group justify="flex-end" mt={"md"}>
-          <Button type="submit">Guardar</Button>
-        </Group>
+        <form
+          onSubmit={form.onSubmit((values) => {
+            form.validate();
+            console.log(values);
+          })}
+        >
+          <SimpleGrid spacing={"xs"}>
+            <TextInput
+              label="Titulo"
+              withAsterisk
+              key={form.key("title")}
+              {...form.getInputProps("title")}
+            />
+            <Textarea
+              label="Contenido"
+              withAsterisk
+              key={form.key("content")}
+              {...form.getInputProps("content")}
+            />
+          </SimpleGrid>
+          <Group justify="flex-end" mt={"md"}>
+            <Button
+              variant="outline"
+              onClick={() => {
+                close();
+                form.reset();
+              }}
+            >
+              Cancelar
+            </Button>
+            <Button type="submit">Guardar</Button>
+          </Group>
+        </form>
       </Modal>
 
       <Button
